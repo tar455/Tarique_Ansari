22669a72d826d73908791a5efa710be91bda8f3e
Tarique <tariqueansari455@gmail.com>
2021-09-01 14:20:05 +0530

    Bug Fixes

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,17 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Feedback from '../component/FeedBack'
 
 export default function About() {
     return (
         <div>
             <div className="outer-content">
                 <div className="inner-content">
-                <p>
-                    Hello, Reader My name is Md Tarique Ansari.
-                    Thanks for visit my portfolio.
-                    Update coming soon.
-                </p>
+                    <p>
+                        Hello, I am Md  Tarique Ansari. I Full Stack MERN  developer.
+                    </p>
                 </div>
             </div>
+            <Link to="/Tarique_Ansari"><button className="btn btn-primary">Back to Home</button></Link>
+            <Feedback />
         </div>
     )
 }
